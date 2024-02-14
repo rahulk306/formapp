1dda27f75ac61dec408bf6bc6303110f67c7f42f
rahulkumar306 <rahulkumar89867@gmail.com>
2024-02-14 23:40:44 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
+import { isValidElement, useState } from 'react';
 
 function App() {
+
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setlastName] = useState("");
+  
+  // console.log("firstName : " + firstName);
+  // console.log("lastName : " + lastName);
+
+  // function firstNamechangeHandler(event){
+  //   setFirstName(event.target.value);
+  //   // console.log(event.target.value);
+  // }
+
+  // function lastNamechangeHandler(event){
+  //   setlastName(event.target.value);
+  //   // console.log(event.target.value);
+  // }
+
+  const [formData, setFormData] = useState({firstName:"", lastName:"", email:"", comments:"", isVisible:true});
+
+  console.log(formData);
+  
+  function changeHandler(event)
+  {
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData, [event.target.name]:event.target.value
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type='text' 
+        placeholder='first name' 
+        onChange={changeHandler} 
+        name='firstName'
+        value={formData.firstName}/>
+
+        <br/><br/>
+
+        <input type='text' 
+        placeholder='last name' 
+        onChange={changeHandler} 
+        name='lastName' v
+        alue={formData.lastName}/>
+
+        <br/><br/>
+
+        <input type='email' 
+        placeholder='your email' 
+        onChange={changeHandler} 
+        name='email' 
+        value={formData.email}/>
+
+        <br/><br/>
+
+        <textarea
+          placeholder='enter your comments here'
+          onChange={changeHandler}
+          name='comments'
+          value={formData.comments}
+        />
+
+        <br/><br/>
+
+        <input
+          type='checkbox'
+          onChange={changeHandler}
+          name='isVisible'
+          value={formData.isVisible}
+          id='isVisible'
+          checked={formData.isVisible}
+        />
+        <label htmlFor='isVisible'>Am I Visible ?</label>
+
+      </form>
     </div>
   );
 }
